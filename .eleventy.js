102f7ba78b60372b88fa48fb2fdf09ef5f0a2ce4
awcodes <adam@adamwgd.com>
2022-04-04 14:06:24 -0400

    wip

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,17 @@
 const UserConfig = require("@11ty/eleventy/src/UserConfig");
+const { DateTime } = require("luxon");
 
 /** @param {UserConfig} config */
 module.exports = function (config) {
   config.addWatchTarget("./src/sass/");
-  config.addPassthroughCopy("./src/_public/**/*");
+
+  config.addPassthroughCopy({ "./src/_public": "/" });
 
   config.addNunjucksFilter("limit", (arr, limit) => arr.slice(0, limit));
+
+  config.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
 
   return {
     htmlTemplateEngine: "njk",
